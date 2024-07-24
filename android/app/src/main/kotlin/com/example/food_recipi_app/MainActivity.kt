51a7e9ef3505db1767a545c68df17f4bf213b2f1
parth sharma <darkcodeing0@gmail.com>
2024-07-24 10:11:49 +0530

    First day design home screen

---
+++ android/app/src/main/kotlin/com/example/food_recipi_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.food_recipi_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:food_recipi_app/activity/constant.dart';
+import 'package:food_recipi_app/activity/home_screen.dart';
+import 'package:image_picker/image_picker.dart';
+
+void main() {
+  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+    statusBarColor: Colors.transparent
+  ));
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primaryColor: kPrimaryColor,
+        fontFamily: "Hellix"
+      ),
+     // debugShowCheckedModeBanner: false,
+      home: Home(),
+    );
+  }
+}
+
